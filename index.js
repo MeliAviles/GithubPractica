44d75e1e443d42b783266a1b99319f3518cbed97
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-08-19 20:54:23 -0300

    maira

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+
+    function laCajaDePandora(numero) {
+        if (numero % 2 === 0) {
+            
+            return numero.toString(2);
+        } else {
+            
+            return numero.toString(16);
+        }
+    }
+
+    function nombreCompleto() {
+        return {
+            nombre: "Maira",
+            edad: 28, 
+            nacionalidad: "Argentina"
+        };
     }
