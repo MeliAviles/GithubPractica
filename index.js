9c45f3a420c87b97291b188bdf124af382c60a12
nanu <nadixpeque@gmail.com>
2024-08-19 22:45:01 -0300

    laTerceraEsLaVencida

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    if(numero % 2 === 0) {
+        return numero.toString(2) + "es par a binario"
+    } else {
+        return numero.toString(2) + "es impar a hexadecimal"
     }
+}
+
+function Nadia(){
+    return {
+        name: "Nadia",
+        age: 25,
+        nationality: "Argentina"
+    }
+}
