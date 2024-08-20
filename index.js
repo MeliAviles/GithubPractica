404cf65d88676c0a741d1dea972f16680d6e6e4e
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-08-19 23:07:04 -0300

    mairaa

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 
     function nombreCompleto() {
         return {
-            nombre: "Maira",
+            nombre: "Mairaa",
             edad: 28, 
             nacionalidad: "Argentina"
         };
